593e7da18d49513002c01618f5d2c5469f5cc447
btcorso <uniebcorso@fei.edu.br>
2022-10-11 00:27:23 -0300

    arrowfunction

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const robotron = document.querySelector('#robotron');
 
-robotron.addEventListener("click", testeLog);
+robotron.addEventListener("click", (evento) => {
+    console.log(evento);
+} );
 
 function testeLog() {
     console.log("teste");
 }
+
+testeLog();
