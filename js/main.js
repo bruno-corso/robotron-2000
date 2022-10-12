54fcd9d93d3e56aa3e73d15bdab454ffe9cf04af
btcorso <uniebcorso@fei.edu.br>
2022-10-12 11:04:27 -0300

    adicionando comentarios

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@ const braco = document.querySelector("#braco");
 
 const controle = document.querySelectorAll(".controle-ajuste");
 
+//forEach vai percorrer cada elemento do meu array
 controle.forEach( (elemento) =>{
+    //em cada elemento do meu array eue stou adicionando um evento 'click'
     elemento.addEventListener("click", (evento)=>{
+        //o evento 'click' vai chamar a funcao manipulaDados
         manipulaDados(evento.target.textContent);
         console.log("click");
     })
 });
 
+//funcao que adiciona se for '+' e subtrai se for '-'
 function manipulaDados(operador) {
     if (operador === "+" ) {
         braco.value = parseInt(braco.value) + 1;
